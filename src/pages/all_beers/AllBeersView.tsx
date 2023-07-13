91f56273a86dd422d707ab28643ff323044df12b
Bishal Karki <bishaldobato@gmail.com>
2023-07-13 18:11:23 +0545

    Add load more function

--- src/pages/all_beers/AllBeersView.tsx
+++ src/pages/all_beers/AllBeersView.tsx
@@ -1,19 +1,38 @@
-import { ReactElement } from 'react';
+import {
+    ReactElement,
+    useCallback,
+    useState,
+} from 'react';
 import CatalogItem from '../../components/CatalogItem.tsx';
 import { useAllBeers } from '../../apis/useApi.js';
 
 
 const AllBeersView = (): ReactElement => {
-    const {data: beers} = useAllBeers(1);
+    const [page, setPage] = useState(1);
+
+    const {data: beers, isLoading} = useAllBeers(page);
+
+    const handleLoadMore = useCallback(() => {
+        setPage(page + 1);
+    }, [page]);
 
     return (
-        <div className="d-flex flex-column gap-4">
-            {beers?.map((item, index) =>
-                <CatalogItem
-                    key={index}
-                    beer={item}
-                />,
-            )}
+        <div>
+            <div className="d-flex flex-column gap-4">
+                {beers?.map((item, index) =>
+                    <CatalogItem
+                        key={index}
+                        beer={item}
+                    />,
+                )}
+            </div>
+
+            <p className="mt-4 text-primary fw-semibold text-center">
+                {isLoading
+                    ? <span>Loading...</span>
+                    : <span role="button" onClick={handleLoadMore}>Load more &#9660;</span>
+                }
+            </p>
         </div>
     );
 };
